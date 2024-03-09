4cc53f0496780c31fd366ad6a7347a7f4241439a
rajandangi <rajandangi649@gmail.com>
2024-03-09 18:52:18 +1030

    Refactor async/await and add Promise methods

--- async-js-promises-&-callbacks/app.js
+++ async-js-promises-&-callbacks/app.js
@@ -20,7 +20,7 @@ const setTimer = (duration) => {
   });
   return promise;
 }
-
+/*
 function trackUserHandler() {
   let positionData;
   getPosition()
@@ -40,6 +40,40 @@ function trackUserHandler() {
   }, 0);// The browser communicates with JS via the Event Loop and the Message Queue to let it know once a longer-taking task finished.
   console.log('Getting position...');
 }
+*/
+
+// Async and Await is a more modern way to handle promises, it's a syntactic sugar over promises.
+async function trackUserHandler() {
+  console.log('Getting position...');
+
+  try {
+    let posData = await getPosition();
+    let timerData = await setTimer(2000);
+    console.log(timerData, posData);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 button.addEventListener('click', trackUserHandler);
 
+/** Promise.race()
+ * ----------------
+ * Race only cares about the first promise that finishes faster, 
+ * it doesn't matter if the other promise is still pending.
+*/
+Promise.race([getPosition(), setTimer(1000)]).then(data => {
+  console.log('First and Faster', data);
+});
+
+// Promise.all() will only resolve if all promises are resolved.
+// if one of the promises is rejected, the whole Promise.all() will be rejected.
+Promise.all([getPosition(), setTimer(1000)]).then(promiseData => {
+  console.log(promiseData);
+});
+
+// Promise.allSettled() will resolve all promises, even if one of them is rejected.
+// It will return an array with the results of all promises.
+Promise.allSettled([getPosition(), setTimer(1000)]).then(promiseData => {
+  console.log(promiseData);
+});
