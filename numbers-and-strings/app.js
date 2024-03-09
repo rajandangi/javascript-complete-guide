ba2fcfada5ef205e2759dfaf808c9ff70c4172e5
rajandangi <rajandangi649@gmail.com>
2024-03-09 15:37:03 +1030

    Add async JavaScript code and regular expressions

--- numbers-and-strings/app.js
+++ numbers-and-strings/app.js
@@ -44,3 +44,33 @@ productPrice = 100;
 const productOutput = productDescription`This product is (${productName}) and it costs ${productPrice}. That's all`;
 console.log(productOutput);
 
+
+// Regular Expressions ("RegEx")
+const regex = /hello/;
+console.log(regex.test('hello'));//true
+console.log(regex.test('Hello'));//false
+console.log(regex.test('hello world'));//true
+console.log('------------------')
+
+const regex1 = /(h|H)ello/;
+console.log(regex1.test('hello'));//true
+console.log(regex1.test('Hello'));//true
+
+console.log('------------------')
+const regex2 = /.ello/;
+console.log(regex2.test('hello'));//true
+console.log(regex2.test('Hello'));//true
+console.log(regex2.test('This is anything mello'));//true
+
+console.log('------------Email Regex--------')
+const emailRegex = /^\S+@\S+.\S+$/; // \-escape character, \S- non-whitespace character, + - one or more
+console.log(emailRegex.test('rajandangi649@gmail.com'));//true
+
+console.log('------------exec function--------')
+console.log(regex.exec('hello'));//[ 'hello', index: 0, input: 'hello', groups: undefined ]
+console.log(regex.exec('Hello'));//null
+console.log(regex.exec('Hello hello'));//[ 'hello', index: 6, input: 'Hello hello', groups: undefined ]
+
+
+console.log('------------or you can use match function--------')
+console.log('hello'.match(regex));//[ 'hello', index: 0, input: 'hello', groups: undefined ]
