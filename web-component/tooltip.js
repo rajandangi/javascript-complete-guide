af64c369d1a47e837c888e11b48e90413fd31f98
rajandangi <rajandangi649@gmail.com>
2024-03-31 23:00:43 +1030

    Add styles and update tooltip behavior in web component

--- web-component/tooltip.js
+++ web-component/tooltip.js
@@ -1,5 +1,9 @@
 // Refer to the README.md for the lifecycle of the custom element
 // Every custom element must extend HTMLElement
+// use ::slotted(*) to style all the elements that are slotted
+// :host is used to style the custom element itself
+// :host(.some-class) is used to style the custom element itself when it has a class of some-class
+// :host-context(p) is used to style the custom element itself when it is inside a p element
 class Tooltip extends HTMLElement {
     constructor() {
         super();// Always call super first in constructor
@@ -10,10 +14,29 @@ class Tooltip extends HTMLElement {
         this.shadowRoot.innerHTML = `
             <style>
             div{
+                font-weight: normal;
                 background-color:black;
                 color:white;
                 position:absolute;
+                top:1.5rem;
+                left:0.75rem;
                 z-index:10;
+                padding:0.15rem;
+                border-radius:3px;
+                box-shadow: 1px 1px 6px rgba(0,0,0,0.26);
+            }
+            :host{
+                position:relative;
+            }
+            :host(.important){
+                background-color: var(--color-primary, #ccc);
+                padding: 0.15rem;
+            }
+            :host-context(p){
+                font-weight: bold;
+            }
+            ::slotted(.highlight){
+                border-bottom: 2px dotted red;
             }
             </style>
             <slot>Some Default</slot>
